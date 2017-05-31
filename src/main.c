09b529a0fe6b0abe593fb68ae9e88356080a31c8
KeyboardFire <andy@keyboardfire.com>
2017-05-31 13:04:57 -0500

    Set the proper locale for Unicode support
    That was shockingly easy.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include "chan.h"
 
+#include <locale.h>
+
 int main(int argc, char **argv) {
+    setlocale(LC_ALL, "");
     struct chan *chan = chan_init();
     chan_main_loop(chan);
     chan_destroy(chan);
